10ef1edd7dc908d638d7f79f0e3f65b9c6a88734
Crisrael M. Lucero <clucero@spawar.navy.mil>
2020-04-01 20:15:09 -0700

    Changing client to bot class

--- venv/bot.py
+++ venv/bot.py
@@ -3,44 +3,41 @@
 # This bot will provide availability management in order to track who is available at what time for each day.
 # Bot URL: https://discordapp.com/api/oauth2/authorize?client_id=694343099714895873&permissions=8&scope=bot
 import os
-import discord
-from .schedule_manager import ScheduleManager
+from discord.ext import commands
+# from .schedule_manager import ScheduleManager
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN') # Create .env file to hold DISCORD_TOKEN
 
-client = discord.Client()
-availability = ScheduleManager()
+bot = commands.Bot(command_prefix='!')
+# availability = ScheduleManager()
 
 def do_nothing():
     print(f'Doing nothing!')
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
 
-@client.event
+@bot.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
-        f'Hi {member.name}, welcome to Reckless!'
+        f'Hi {member.name}, welcome to Discord!'
     )
 
-@client.event
+@bot.event
 async def on_reaction_add(reaction, user):
     do_nothing()
 
-@client.event
+@bot.event
 async def on_reaction_remove(reaction, user):
     do_nothing()
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content == '!squad_availability':
-        response = 'Filler Text!'
-        await message.channel.send(response)
+@bot.command(name='squad_availability', help='Gives current availbility on clan members')
+async def squad_avail(ctx):
+    response = 'Filler Text!'
+    await ctx.send(response)
 
-client.run(TOKEN)
+bot.run(TOKEN)
